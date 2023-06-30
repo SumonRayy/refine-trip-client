16338243dec2fb3df32a54086da27359493eb8bd
Sumon Roy <sumon.roy@thewitslab.com>
2023-06-30 20:26:29 +0530

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,139 @@
+import {
+  AuthBindings,
+  GitHubBanner,
+  Refine,
+  WelcomePage,
+} from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import dataProvider, { GraphQLClient } from "@refinedev/graphql";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import axios, { AxiosRequestConfig } from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { CredentialResponse } from "./interfaces/google";
+import { parseJwt } from "./utils/parse-jwt";
+
+const API_URL = "https://your-graphql-url/graphql";
+
+const client = new GraphQLClient(API_URL);
+const gqlDataProvider = dataProvider(client);
+const axiosInstance = axios.create();
+axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
+  const token = localStorage.getItem("token");
+  if (request.headers) {
+    request.headers["Authorization"] = `Bearer ${token}`;
+  } else {
+    request.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+  return request;
+});
+
+function App() {
+  const authProvider: AuthBindings = {
+    login: async ({ credential }: CredentialResponse) => {
+      const profileObj = credential ? parseJwt(credential) : null;
+
+      if (profileObj) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            ...profileObj,
+            avatar: profileObj.picture,
+          })
+        );
+
+        localStorage.setItem("token", `${credential}`);
+
+        return {
+          success: true,
+          redirectTo: "/",
+        };
+      }
+
+      return {
+        success: false,
+      };
+    },
+    logout: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token && typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        axios.defaults.headers.common = {};
+        window.google?.accounts.id.revoke(token, () => {
+          return {};
+        });
+      }
+
+      return {
+        success: true,
+        redirectTo: "/login",
+      };
+    },
+    onError: async (error) => {
+      console.error(error);
+      return { error };
+    },
+    check: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        return {
+          authenticated: true,
+        };
+      }
+
+      return {
+        authenticated: false,
+        error: {
+          message: "Check failed",
+          name: "Token not found",
+        },
+        logout: true,
+        redirectTo: "/login",
+      };
+    },
+    getPermissions: async () => null,
+    getIdentity: async () => {
+      const user = localStorage.getItem("user");
+      if (user) {
+        return JSON.parse(user);
+      }
+
+      return null;
+    },
+  };
+
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <Refine
+          dataProvider={gqlDataProvider}
+          routerProvider={routerBindings}
+          authProvider={authProvider}
+          options={{
+            syncWithLocation: true,
+            warnWhenUnsavedChanges: true,
+          }}
+        >
+          <Routes>
+            <Route index element={<WelcomePage />} />
+          </Routes>
+          <RefineKbar />
+          <UnsavedChangesNotifier />
+          <DocumentTitleHandler />
+        </Refine>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
